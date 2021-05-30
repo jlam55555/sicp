12a2cf3a83fd0a94ebd83c7007d2b1e48b5fbdbe
Jonathan Lam <jlam55555@gmail.com>
2021-05-29 20:27:40 -0400

    continue working on 1.2

--- 1.2/1.2.scm
+++ 1.2/1.2.scm
@@ -26,3 +26,53 @@
 ;;; to the syntactic structure, while the former refers to the flow of
 ;;; execution and state. The above example is a recursive procedure but not a
 ;;; recursive process.
+
+;;; tree-recursive version of fib; bad
+(define (fib n)
+  (cond ([zero? n] 0)
+	([= n 1] 1)
+	(else (+ (fib (- n 1))
+		 (fib (- n 2))))))
+
+;;; binet's formula; write power using exp/log
+(define (pow a b)
+  (exp (* b (log a))))
+(define phi
+  (/ (+ 1 (sqrt 5))
+     2))
+(define (fib-2 n)
+  (exact (round (/ (pow phi n)
+		   (sqrt 5)))))
+
+;;; iterative version; rewritten to use let. Uses the realization that the state
+;;; of the fibonacci process can be stored in three variables
+(define (fib-3 n)
+  (let fib-iter ([a 1]
+		 [b 0]
+		 [count n])
+    (if [zero? count]
+	b
+	(fib-iter (+ a b)
+		  a
+		  (1- count)))))
+
+;;; testcases; first version is very slow for numbers a little larger than this
+(define (tc-iota fn n)
+  (map fn (iota n)))
+(tc-iota fib 30)
+(tc-iota fib-2 30)
+(tc-iota fib-3 30)
+
+;;; counting change; rewritten to use a list
+(define (count-change amount)
+  (let ([coins '(1 5 10 25 50)])
+    (let cc-iter ([amount amount]
+		  [kinds-of-coins (1- (length coins))])
+      (cond ([= amount 0] 1)
+	    ([or (< amount 0)
+		 (< kinds-of-coins 0)]
+	     0)
+	    (else (+ (cc-iter amount
+			      (1- kinds-of-coins))
+		     (cc-iter (- amount (list-ref coins kinds-of-coins))
+			      kinds-of-coins)))))))
---
+++ 1.2/exercises.scm
@@ -0,0 +1,158 @@
+;;; exercises from sicp 1.2
+;;; as with 1.1, this may depend on some code from 1.2.scm (e.g., testcase code)
+(load "1.2.scm")
+
+;;; 1.9
+
+;;; assume a,b>0 and that inc, dec are defined
+(define (+v1 a b)
+  (if [= a 0]
+      b
+      (inc (+v1 (dec a) b))))
+
+(define (+v2 a b)
+  (if [= a 0]
+      b
+      (+v1 (dec a) (inc b))))
+
+;;; process for +v1:
+;;; (commented b/c inc/dec aren't defined)
+;; (+v1 4 5)
+;; (inc (+v1 (dec 4) 5))
+;; (inc (+v1 3 5))
+;; (inc (inc (+v1 (dec 3) 5)))
+;; (inc (inc (+v1 2 5)))
+;; (inc (inc (inc (+v1 (dec 3) 5))))
+;; (inc (inc (inc (+v1 2 5))))
+;;; etc; linear recursion
+
+;;; process for +v2:
+;; (+v2 4 5)
+;; (+v2 (dec 4) (inc 5))
+;; (+v2 3 6)
+;; (+v2 (dec 3) (inc 6))
+;; (+v2 2 7)
+;;; etc; linear iteration
+
+;;; 1.10
+;;; Ackermann's function
+(define (A x y)
+  (cond ([= y 0] 0)
+	([= x 0] (* 2 y))
+	([= y 1] 2)
+	(else (A (1- x)
+		 (A x (1- y))))))
+
+(A 1 10)
+;; (A 0 (A 1 9))
+;; (A 0 (A 0 (A 1 8)))
+;; (A 0 (A 0 (A 0 (A 1 7))))
+;; (A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+;; (A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+;; (A 0 (A 0 (A 0 (A 0 64))))
+;; (A 0 (A 0 (A 0 128)))
+;; (A 0 (A 0 256))
+;; (A 0 512)
+;; 1024
+
+;;; seems like (A 1 n) => 2^n
+
+(A 2 4)
+;; (A 1 (A 2 3))
+;; (A 1 (A 1 (A 2 2)))
+;; (A 1 (A 1 (A 1 (A 2 1))))
+;; (A 1 (A 1 (A 1 2)))
+;; (A 1 (A 1 (A 0 (A 1 1))))
+;; (A 1 (A 1 (A 0 2)))
+;; (A 1 (A 1 4))
+;; ;;; (using observation above that (A 1 n) => 2^n)
+;; (A 1 16)
+;; ;;; (using that observation again)
+;; 65536
+
+;;; (A 2 1) => 2
+;;; (A 2 2) => 2^2 = 4
+;;; (A 2 3) => 2^4 = 16
+;;; (A 2 4) => 2^16 = 65536
+;;; (A 2 1) => 2^65536 = ???
+
+(A 3 3)
+;; (A 2 (A 3 2))
+;; (A 2 (A 2 (A 3 1)))
+;; (A 2 (A 2 2))
+;; (A 2 (A 1 (A 2 1)))
+;; (A 2 (A 1 2))
+;; (A 2 4)
+;; ;;; using previous answer
+;; 65536
+
+(define (f n) (A 0 n))			; 2*n
+(define (g n) (A 1 n))			; 2^n
+(define (h n) (A 2 n))			; ^n(2) (tetration)
+
+;;; 1.11
+;;; not exactly tribonacci, but close enough
+
+;;; recursive process
+(define (trib n)
+  (if [< n 3]
+      n
+      (+ (trib (- n 1))
+	 (* 2 (trib (- n 2)))
+	 (* 3 (trib (- n 3))))))
+
+;;; iterative process
+(define (trib-2 n)
+  (if [< n 3]
+      n
+      (let iter ([a 2] [b 1] [c 0] [n (- n 2)])
+	(if [zero? n]
+	    a
+	    (iter (+ a (* 2 b) (* 3 c))
+		  a
+		  b
+		  (1- n))))))
+
+;;; defined in 1.2.scm; first version is already getting a little bit slow
+(tc-iota trib 30)
+(tc-iota trib-2 30)
+
+;;; 1.12: Pascal's triangle
+;;; very simple version without memoization, akin to the other examples so far
+;;; - row: 1-indexed row; col: 1-indexed position in row (col <= row)
+(define (pascal row col)
+  (if [or (< row 1)
+	  (< col 1)
+	  (> col row)]
+      -1
+      (let rec ([row row]
+		[col col])
+	(if [or (= col 1) (= col row)]
+	    1
+	    (+ (rec (1- row) (1- col))
+	       (rec (1- row) col))))))
+
+;;; generate a row of pascal's triangle using the above method
+(define (pascal-row row)
+  (map (lambda (col) (pascal row (1+ col)))
+       (iota row)))
+
+;;; generate the first n rows of pascals triangle
+(define (pascal-tri rows)
+  (map pascal-row
+       (map 1+ (iota rows))))
+
+(pascal-tri 5)
+
+;;; 1.13: Proof of Binet's formula
+;;; I did this at some point in the past:
+;;; http://eis.lambdalambda.ninja/posts/proof-of-binets-formula
