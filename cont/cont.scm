3260666c18485969d0be1f030027d0c77a494651
Jonathan Lam <jlam55555@gmail.com>
2021-08-16 01:50:28 -0400

    added implementation of call/cc

--- cont/cont.scm
+++ cont/cont.scm
@@ -132,13 +132,79 @@
   (let ([input (read)])
     (mi::cont-eval input
 		   mi::*global-environment*
-		   mi::user-print)
-    (mi::driver-loop)))
+		   (lambda (val)
+		     (mi::user-print val)
+		     (mi::driver-loop)))))
 
 ;;; FINISHED SIMPLE EXAMPLE
 
-;;; TODO: implement call/cc (expose continuation)
+(define (mi::analyze exp)
+  ;; add call/cc clause
+  (cond ([mi::self-evaluating? exp]
+	 (mi::analyze-self-evaluating exp))
+	([mi::quoted? exp]
+	 (mi::analyze-quoted exp))
+	([mi::variable? exp]
+	 (mi::analyze-variable exp))
+	([mi::assignment? exp]
+	 (mi::analyze-assignment exp))
+	([mi::definition? exp]
+	 (mi::analyze-definition exp))
+	([mi::if? exp]
+	 (mi::analyze-if exp))
+	([mi::lambda? exp]
+	 (mi::analyze-lambda exp))
+	([mi::begin? exp]
+	 (mi::analyze-sequence (mi::begin-actions exp)))
+	([mi::cond? exp]
+	 (mi::analyze (mi::cond->if exp)))
+
+	;; this is new
+	([mi::call/cc? exp]
+	 (mi::analyze-call/cc exp))
+	
+	([mi::application? exp]
+	 (mi::analyze-application exp))
+	(#t
+	 (error 'mi::analyze "unknown expression type" exp))))
+
+(define (mi::call/cc? exp)
+  ;; whether exp is a call/cc expression
+  (mi::tagged-list? exp 'call/cc))
+
+(define (mi::call/cc-arg exp)
+  ;; get argument of call/cc (should be the second and last subexpression
+  ;; of `exp`)
+  (cadr exp))
+
+;;; What we want to achieve (CPS form):
+;; (call/cc f ret)
+;;      =>
+;; (define (call/cc f ret)
+;;   (f (lambda (x _) (ret x)) ret))
+;;   
+;;; Same without CPS form (note that ret comes from the interpreter and is not
+;;; usually exposed):
+;; (call/cc f)
+;;      =>
+;; (define (call/cc f)
+;;   (f ret))
+
+(define (mi::analyze-call/cc exp)
+  ;; for (call/cc f), call f, binding the continuation as a primitive
+  ;; procedure to the argument of f
+  (let ([fproc (mi::analyze (mi::call/cc-arg exp))])
+    (lambda (env ret)
+      (fproc env
+	     (lambda (proc)
+	       (mi::execute-application
+		proc
+		(list (list 'primitive ret))
+		ret))))))
 
 ;;; TODO: examples of call/cc
+;; (call/cc (lambda (r1) (call/cc (lambda (r2) (r1 (r2 (r1 3)))))))
+;; (+ 2 (call/cc (lambda (ret) (ret 4) 5)))
+;; (+ 2 (call/cc (lambda (ret) 5)))
 
 ;;; TODO: multiple continuations (optional error continuation)
